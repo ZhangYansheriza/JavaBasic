d480cfb8de32b060e3c031d4f30544e48658ed2b
Zhangyan <15ts0040@esp-g.com>
2017-09-20 13:18:17 +0900

    PTra02_01まで完了

--- Practice/src/practice02/PTra02_01.java
+++ Practice/src/practice02/PTra02_01.java
@@ -11,12 +11,25 @@ public class PTra02_01 {
 
 		// ★ 20を5で割った値を出力してください
 
+		int a;
+		a=20;
+		a/=5;
+		System.out.println(a);
 
 		// ★ 126に13を掛けた値を出力してください
+
+		int b;
+		b=126;
+		b*=13;
+		System.out.println(b);
 
 
 		// ★ 97を6で割ったときの余りを出力してください
 
+		int c;
+		c=97;
+		c%=6;
+		System.out.println(c);
 
 	}
 }
