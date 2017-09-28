67a01a16d2c3ff7d25fb0c559997f217b70c3ab7
Zhangyan <15ts0040@esp-g.com>
2017-09-28 14:34:01 +0900

    PTra03_07まで完了

--- Practice/src/practice03/PTra03_05.java
+++ Practice/src/practice03/PTra03_05.java
@@ -34,7 +34,7 @@ public class PTra03_05 {
 		 * ※ プログラムは何行書いても良いです
 		 */
 		if ( num>100|| num<0) {
-			System.out.println("不正な値が入力されました");                                  
+			System.out.println("不正な値が入力されました");
 		}else if(num >= 75){
 			System.out.println("雨が降る確率が高いです");
 		} else if(num>=30){
@@ -44,4 +44,3 @@ public class PTra03_05 {
 	}
 	}
 }
-	
