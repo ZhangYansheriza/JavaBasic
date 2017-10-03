a575e85f6e40d497add0292750f1a6c41482342e
Zhangyan <15ts0040@esp-g.com>
2017-10-03 15:30:15 +0900

    PTra05_04まで完了

--- Practice/src/practice05/PTra05_04.java
+++ Practice/src/practice05/PTra05_04.java
@@ -33,6 +33,26 @@ public class PTra05_04 {
 				break;
 			case "1":
 				System.out.println("月曜");
+				break;
+			case "2":
+				System.out.println("火曜日");
+				break;
+			case "3":
+				System.out.println("水曜日");
+				break;
+			case "4":
+				System.out.println("木曜日");
+				break;
+			case "5":
+				System.out.println("金曜日");
+				break;
+			case "6":
+				System.out.println("土曜日");
+				break;
+			default:
+				System.out.println("①");
+				break;
+
 		}
 	}
 }
