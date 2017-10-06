f56fcd3af204dbd5aeb60355bc686b92f7bbd3c5
Zhangyan <15ts0040@esp-g.com>
2017-10-06 15:42:53 +0900

    PTra07_03まで完了

--- Practice/src/practice07/PTra07_03.java
+++ Practice/src/practice07/PTra07_03.java
@@ -10,9 +10,12 @@ public class PTra07_03 {
 	public static void main(String[] args) {
 
 		// ★ char型の配列keywordを定義し 「'平', '島', '沼', '山', '森'」 で初期化してください
+		char k[]= {'平', '島', '沼', '山', '森'};
 
 
 		// ★ 配列keywordに格納されている値を後ろから順番に全て表示してください
-
+		for(int j = k.length - 1; j >= 0;j--) {
+			System.out.println(k[j]);
+		}
 	}
 }
